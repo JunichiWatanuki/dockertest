1ae050fdd943072aff30be566bc26698f7044c0b
Junichi Watanuki <junichi.watanuki@gmail.com>
2020-08-23 19:50:29 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,26 @@ echo "Test $2"
 time=$(date)
 echo "::set-output name=time::$time"
 
+
+ARRUSERLIST+=($2)
+
 for item in ${ARRUSERLIST[@]}; do
     echo "ARRUSERLIST = ${item}"
 done
+
+
+## Started timestamp
+STARTEDTIMESTAMP=`date '+%Y%m%d-%T' | sed -e 's/\://g'`
+CURRENTDIR=`pwd`
+INIFILE="${CURRENTDIR}/${STARTEDTIMESTAMP}.ini"
+echo "[main]" > "${INIFILE}"
+echo "  ##### parameter file" >> "${INIFILE}"
+echo "[required]" >> "${INIFILE}"
+echo "  STARTEDTIMESTAMP=\"${STARTEDTIMESTAMP}\"" >> "${INIFILE}"
+echo "  WORKINGDIR=\"${OUTPUTDIR}\"" >> "${INIFILE}"
+echo "  ARRUSERLIST=\"${ARRUSERLIST[@]}\"" >> "${INIFILE}"
+echo "  INIFILE=\"${INIFILE}\"" >> "${INIFILE}"
+echo "[optional]" >> "${INIFILE}"
+echo "  SLACKNOTIFY=\"${PROXYINFO}\"" >> "${INIFILE}"
+
+cat ${INIFILE}
